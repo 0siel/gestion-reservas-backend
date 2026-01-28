fe5e614db8981ae882dbd73f7c2c6134d4a971a5
unknown <ggsuak@gmail.com>
2026-01-27 23:22:06 -0600

    Añadí archivo HabitacionClient

---
+++ microservicios/gestion-reservas-commons/src/main/java/com/example/demo/clients/HabitacionClient.java
@@ -0,0 +1,28 @@
+package com.example.demo.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.dto.HabitacionRequest;
+import com.example.demo.dto.HabitacionResponse;
+
+import java.util.List;
+
+@FeignClient(name = "msv.habitaciones")
+public interface HabitacionClient {
+
+	@GetMapping("/")
+	List<HabitacionResponse> listarHabitaciones();
+
+	@GetMapping("/{id}")
+	HabitacionResponse obtenerHabitacionPorId(@PathVariable Long id);
+
+	@GetMapping("/id-habitacion/{id}")
+	HabitacionResponse obtenerHabitacionSinEstado(@PathVariable Long id);
+
+	@PutMapping("/{id}")
+	HabitacionResponse actualizarHabitacion(@PathVariable Long id, @RequestBody HabitacionRequest request);
+}
