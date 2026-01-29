08fd0c578e44f4bef213cc9bd5fa7fcf0de1f5ad
MahiliFlores <mahiliflores@outlook.com>
2026-01-28 23:13:07 -0600

    Mahili funciona

--- microservicios/msv-reservas/src/main/java/msv_reservas/controllers/ReservaController.java
+++ microservicios/msv-reservas/src/main/java/msv_reservas/controllers/ReservaController.java
@@ -2,16 +2,12 @@ package msv_reservas.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.controllers.CommonController;
 import com.example.demo.dto.ReservaRequest;
 import com.example.demo.dto.ReservaResponse;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import msv_reservas.services.ReservaService;
 
 
